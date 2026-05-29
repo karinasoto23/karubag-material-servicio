1b6ba46b740aa9995593546a29b334d542d8ef14
Karina Soto <karinafono.23@gmail.com>
2026-05-28 20:28:30 -0400

    feat: scaffold inicial material-servicio con configuracion Neon

---
+++ src/main/java/cl/karubag/material/MaterialServicioApplication.java
@@ -0,0 +1,13 @@
+package cl.karubag.material;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialServicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaterialServicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/karubag/material/MaterialServicioApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.karubag.material;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterialServicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
